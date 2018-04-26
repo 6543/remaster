1c4dc6ac567b9bea1ce089aff5417f75d6a7bb60
6543 <dasdfsafdsafdas453@freenet.de>
2018-04-26 19:46:17 +0200

    create docu p2

--- gendeb.sh
+++ gendeb.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 # at the moment only generate dir structure in /build
 
+# setup build dir
+rootfs=build
 echo "clear build"
-[ -d ./build ] && rm -v -R ./build
-mkdir build
+[ -d $rootfs ] && rm -v -R $rootfs
+mkdir $rootfs
 
 echo "copy files"
 ## skripte copieren
+# remaster
+mkdir -p $rootfs/usr/bin/
+cp -v src/remaster.sh $rootfs/usr/bin/remaster
+chmod +x $rootfs/usr/bin/remaster
+# modules
+mkdir -p $rootfs/usr/lib/remaster/
+#...
 
 
 # Pfade anpassen
