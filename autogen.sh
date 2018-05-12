7aadb795fff967a420faa26f9723a68c1d18d155
6543 <dasdfsafdsafdas453@freenet.de>
2018-05-12 20:19:12 +0200

    autogen irreführende namen geändert

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-# at the moment only generate dir structure in /build
+#  install | clean | debug | build-deb
 
-#make ...
 function clean() {
   echo "clear build"
   [ -d build ] && rm -v -R build
   mkdir build
 }
-function build() {
-  echo "build ..."
-  ## skripte copieren ##
+function copy() {
+  echo "copy files ..."
   # remaster
   mkdir -p build/usr/bin/
   cp -v src/remaster.sh build/usr/bin/remaster
@@ -49,21 +47,21 @@ function set_libdir() {
 #modes
 function debug() {
   clean
-  build
+  copy
   set_rootdir "`pwd`/build"
   set_libdir "`pwd`/build/usr/lib/remaster"
 }
 function install() {
   clean
-  build
+  copy
   set_rootdir ""
   set_libdir "/usr/lib/remaster"
   #cp -f -r build/* /
 }
-function build_deb() {
+function build-deb() {
   clean
   #prebuild
-  build
+  copy
   set_rootdir ""
   set_libdir "/usr/lib/remaster"
   ####
@@ -97,13 +95,13 @@ case "$1" in
   debug)
     debug || exit 1
     ;;
-  build)
-    build || exit 1
+  copy)
+    copy || exit 1
     ;;
-  build_deb)
-    build_deb || exit 1
+  build-deb)
+    build-deb || exit 1
     ;;
   *)
-    echo "Usage: install | clean | debug | build_deb"
+    echo "Usage: install | clean | debug | build-deb"
     exit 1
 esac
